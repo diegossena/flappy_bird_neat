01e6a76700cc8cb37a2d15bc7126ae1cedc66487
Diego <diego.sena@grupojrb.com.br>
2022-10-30 14:41:44 -0300

    30/10 + neat

--- __init__.py
+++ __init__.py
@@ -4,79 +4,81 @@ import neat
 from utils import sprite_get
 from config import SCREEN_HEIGHT, SCREEN_WIDTH, FONT_SIZE, NEAT_IS_RUNNING
 
-from Bird import Bird
-from Pipe import Pipe
-from Floor import Floor
+from entities import Bird, Floor, Pipe
 
-def main(neat_generation = 0):
-  # Fonts
+def main():
   pygame.font.init()
-  FONT_ARIAL = pygame.font.SysFont('arial', FONT_SIZE)
-  # entities
-  floor = Floor()
-  birds = [Bird(350)]
-  pipes = [Pipe(700)]
-  # setup
+  neat_generation=1
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  score = 0
+  text_font=pygame.font.SysFont('arial', FONT_SIZE)
   clock = pygame.time.Clock()
-  # run
-  while len(birds):
-    clock.tick(30)
-    # handle inputs
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        pygame.quit()
-        quit()
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-          for bird in birds:
-            bird.jump()
-    # entities_update
-    for bird in birds:
-      bird.update()
+  # static_entities
+  floor = Floor()
+  # game_start
+  while 1:
+    # setup    
+    score = 0
+    # generation_entities
+    pipes = [Pipe(700)]
+    birds = [Bird(350)]
+    # generation_run
+    while len(birds):
+      clock.tick(30)
+      # handle inputs
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          quit()
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_SPACE:
+            for bird in birds:
+              bird.jump()
+      # entities_update
+      for bird in birds:
+        bird.update()
 
-    floor.update()
-    adding_pipe = False
-    pipes_to_remove = []
-    for pipe in pipes:
+      floor.update()
+      adding_pipe = False
+      pipes_to_remove = []
+      for pipe in pipes:
+        for i, bird in enumerate(birds):
+          if pipe.collission(bird):
+            birds.pop(i)
+          if not pipe.passed and bird.x > pipe.x:
+            pipe.passed = True
+            adding_pipe = True
+        pipe.update()
+        if pipe.x + pipe.CANO_TOPO.get_width() < 0:
+          pipes_to_remove.append(pipe)
+
+      if adding_pipe:
+        score += 1
+        pipes.append(Pipe(600))
+
+      for pipe in pipes_to_remove:
+        pipes.remove(pipe)
+
       for i, bird in enumerate(birds):
-        if pipe.collission(bird):
+        if (bird.y + bird.sprite.get_height()) > floor.y or bird.y < 0:
           birds.pop(i)
-        if not pipe.passed and bird.x > pipe.x:
-          pipe.passed = True
-          adding_pipe = True
-      pipe.update()
-      if pipe.x + pipe.CANO_TOPO.get_width() < 0:
-        pipes_to_remove.append(pipe)
+      # screen_draw
+      screen.blit(sprite_get('bg.png'), (0, 0))
 
-    if adding_pipe:
-      score += 1
-      pipes.append(Pipe(600))
+      for bird in birds:
+        bird.draw(screen)
+      for pipe in pipes:
+        pipe.draw(screen)
 
-    for pipe in pipes_to_remove:
-      pipes.remove(pipe)
+      text = text_font.render(f"Pontuação: {score}", 1, (255, 255, 255))
+      screen.blit(text, (SCREEN_WIDTH - 10 - text.get_width(), 10))
+      if NEAT_IS_RUNNING:
+        text = text_font.render(f"Geração: {neat_generation}", 1, (255, 255, 255))
+        screen.blit(text, (10, 10))
 
-    for i, bird in enumerate(birds):
-      if (bird.y + bird.sprite.get_height()) > floor.y or bird.y < 0:
-        birds.pop(i)
-    # screen_draw
-    screen.blit(sprite_get('bg.png'), (0, 0))
-
-    for bird in birds:
-      bird.draw(screen)
-    for pipe in pipes:
-      pipe.draw(screen)
-
-    text = FONT_ARIAL.render(f"Pontuação: {score}", 1, (255, 255, 255))
-    screen.blit(text, (SCREEN_WIDTH - 10 - text.get_width(), 10))
-    if NEAT_IS_RUNNING:
-      text = FONT_ARIAL.render(f"Geração: {neat_generation}", 1, (255, 255, 255))
-      screen.blit(text, (10, 10))
-
-    floor.draw(screen)
-    pygame.display.update()
-  main(neat_generation + 1)
+      floor.draw(screen)
+      pygame.display.update()
+    # on fail
+    neat_generation += 1
 
 if __name__ == '__main__':
   main()
---
+++ entities/__init__.py
@@ -0,0 +1,3 @@
+from .Bird import Bird
+from .Floor import Floor
+from .Pipe import Pipe
